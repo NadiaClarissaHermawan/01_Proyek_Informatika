f36594830ae60d3cba545ba13971eb3cc69408f3
Nadia Clarissa Hermawan <6181901013@student.unpar.ac.id>
2022-02-28 17:07:34 +0700

    query added, main program sudah dapat di run.

--- src/main/java/ProIF01/mainClass/repository/trainRepository.java
+++ src/main/java/ProIF01/mainClass/repository/trainRepository.java
@@ -4,6 +4,8 @@ import ProIF01.mainClass.model.train;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface trainRepository extends JpaRepository<train, Long> {
 
