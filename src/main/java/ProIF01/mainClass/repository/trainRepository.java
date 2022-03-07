40a7bd0db8baf46b872146cb440ab896e628bc16
ReynaldiLukasYudawinata <reynaldi525@gmail.com>
2022-03-07 09:27:27 +0700

    rubah password disesuaikan dengan password database lukas

--- src/main/java/ProIF01/mainClass/repository/trainRepository.java
+++ src/main/java/ProIF01/mainClass/repository/trainRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface trainRepository extends JpaRepository<train, Long> {
-    public List<train> viewAllTrain();
-    public train viewTrainDetail(Long id);
+//    public List<train> viewAllTrain();
+//    public train viewTrainDetail(Long id);
 }
