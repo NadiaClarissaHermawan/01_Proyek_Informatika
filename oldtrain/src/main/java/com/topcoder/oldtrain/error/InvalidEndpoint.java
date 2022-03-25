b63e4e047fbab2a5e25f213e30489e5a3044ba7a
unknown <reynaldi525@gmail.com>
2022-03-25 12:05:10 +0700

    commit

--- oldtrain/src/main/java/com/topcoder/oldtrain/error/InvalidEndpoint.java
+++ oldtrain/src/main/java/com/topcoder/oldtrain/error/InvalidEndpoint.java
@@ -10,12 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Controller
 public class InvalidEndpoint implements ErrorController {
     @RequestMapping("/error")
-    public ResponseEntity<ErrorJson> handleError(HttpServletRequest request) {
-        ErrorJson errorJson = new ErrorJson("failed when edit train");
+    public ResponseEntity<ErrorJson> handleError(HttpServletRequest request){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        //System.out.println("masuk error");
+        ErrorJson errorJson = new ErrorJson("invalid endpoint");
+        if (status != null) {
+            //System.out.println("masuk if");
+            Integer statusCode = Integer.valueOf(status.toString());
+            System.out.println(statusCode);
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return new ResponseEntity<>(errorJson, HttpStatus.METHOD_NOT_ALLOWED);
+            }
+            else if (statusCode== HttpStatus.BAD_REQUEST.value()){
+                System.out.println("jackpot");
+                ErrorJson response= new ErrorJson("failed when edit train");
+                return new ResponseEntity<>(response, BAD_REQUEST);
+            }
 
-        return new ResponseEntity<>(errorJson, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(errorJson, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
